35be6952b05d16f2faae89444cd8f1642d0e9d73
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2017-07-10 15:49:49 +0800

    BeanUtils的使用

--- spring-beans/src/test/java/org/springframework/beans/factory/access/SingletonBeanFactoryLocatorTests.java
+++ spring-beans/src/test/java/org/springframework/beans/factory/access/SingletonBeanFactoryLocatorTests.java
@@ -189,51 +189,26 @@ public class SingletonBeanFactoryLocatorTests {
 
 
 class TestBean {
-
 	private String name;
-
 	private List<?> list;
-
 	private Object objRef;
 
-	/**
-	 * @return Returns the name.
-	 */
+	// getter and setter ...
 	public String getName() {
 		return name;
 	}
-
-	/**
-	 * @param name The name to set.
-	 */
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	/**
-	 * @return Returns the list.
-	 */
 	public List<?> getList() {
 		return list;
 	}
-
-	/**
-	 * @param list The list to set.
-	 */
 	public void setList(List<?> list) {
 		this.list = list;
 	}
-
-	/**
-	 * @return Returns the object.
-	 */
 	public Object getObjRef() {
 		return objRef;
 	}
-
-	/**
-	 * @param object The object to set.
-	 */
 	public void setObjRef(Object object) {
 		this.objRef = object;
 	}
