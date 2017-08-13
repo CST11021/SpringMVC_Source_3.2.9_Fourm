986d05afcef2443fbe526c9dbdeb1ff1cb81685b
wanghz <l>
2017-08-13 10:25:29 +0800

    测试使用Servlet3.0支持的注解

--- src/main/java/com/whz/web/servlet/MyServlet.java
+++ src/main/java/com/whz/web/servlet/MyServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 
+// 注意：servlet规范中3.0以上版本才支持注解，并且Tomcat需要7以上版本
 @WebServlet(name = "MyServlet", urlPatterns = { "/my" })
 public class MyServlet implements Servlet {
     
