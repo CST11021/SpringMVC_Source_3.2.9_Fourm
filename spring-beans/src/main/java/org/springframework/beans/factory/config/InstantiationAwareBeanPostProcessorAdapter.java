a641100ea190f8bf1776f1aee3fa45e66e1d7020
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2017-11-09 10:00:23 +0800

    BeanFactory生命周期接口方法添加注释

--- spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessorAdapter.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessorAdapter.java
@@ -39,37 +39,55 @@ import org.springframework.beans.PropertyValues;
  */
 public abstract class InstantiationAwareBeanPostProcessorAdapter implements SmartInstantiationAwareBeanPostProcessor {
 
+	// SmartInstantiationAwareBeanPostProcessor 接口
 	public Class<?> predictBeanType(Class<?> beanClass, String beanName) {
 		return null;
 	}
-
 	public Constructor<?>[] determineCandidateConstructors(Class<?> beanClass, String beanName) throws BeansException {
 		return null;
 	}
-
 	public Object getEarlyBeanReference(Object bean, String beanName) throws BeansException {
 		return bean;
 	}
 
+
+
+
+
+	// InstantiationAwareBeanPostProcessor 接口：
+
+	// bean实例化前被调用
 	public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
 		return null;
 	}
-
+	// bean实例化后被调用
 	public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
 		return true;
 	}
-
-	public PropertyValues postProcessPropertyValues(
-			PropertyValues pvs, PropertyDescriptor[] pds, Object bean, String beanName)
-			throws BeansException {
+	// 在工厂将给定的属性值应用到给定的bean之前。允许检查是否已满足所有依赖项，例如基于bean属性设置器上的“Required”注解。
+	// 还允许替换属性值，通常通过创建一个基于原始属性值的MutablePropertyValues实例，添加或删除特定的值
+	// 该方法完成其他定制的一些依赖注入，如：
+	// AutowiredAnnotationBeanPostProcessor执行@Autowired注解注入
+	// CommonAnnotationBeanPostProcessor执行@Resource等注解的注入
+	// PersistenceAnnotationBeanPostProcessor执行@ PersistenceContext等JPA注解的注入
+	// RequiredAnnotationBeanPostProcessor执行@Required注解的检查等等
+	// checkDependencies：依赖检查
+	// applyPropertyValues：应用明确的setter属性注入
+	public PropertyValues postProcessPropertyValues(PropertyValues pvs, PropertyDescriptor[] pds, Object bean, String beanName) throws BeansException {
 
 		return pvs;
 	}
 
+
+
+
+	// BeanPostProcessor 接口：
+
+	// Bean 调用构造函数，实例化之前执行该方法
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		return bean;
 	}
-
+	// Bean 调用构造函数，实例化之后执行该方法
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 		return bean;
 	}
