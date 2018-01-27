4d5054d62089a388bea540945a29504e65bb1b68
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2018-01-27 11:56:47 +0800

    Spring AutoProxy自动代理实现机制

--- spring-aop/src/test/java/com/whz/aop/autoproxy/WaiterImpl.java
+++ spring-aop/src/test/java/com/whz/aop/autoproxy/WaiterImpl.java
@@ -6,6 +6,10 @@ import com.whz.aop.advice.Waiter;
 //定义一个目标类
 public class WaiterImpl implements Waiter {
 
+   // 被自动代理后，该属性无法再被注入了，InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation方法返回代理
+   // 类后，IOC就直接返回Bean实例了，后续的属性注入则无法再被注入了
+   private String testString;
+
    public void greetTo(String name) {
       System.out.println("greet to "+name+"...");
    }
@@ -17,5 +21,13 @@ public class WaiterImpl implements Waiter {
    // 非接口方法无法被代理
    public void sayHello(String name) {
       System.out.println("hello "+name+"...");
+   }
+
+   public String getTestString() {
+      return testString;
+   }
+
+   public void setTestString(String testString) {
+      this.testString = testString;
    }
 }
