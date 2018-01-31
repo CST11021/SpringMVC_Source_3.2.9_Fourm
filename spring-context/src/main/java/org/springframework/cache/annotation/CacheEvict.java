baa590eba4038b13119b30458599dd5d816119ef
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2018-01-31 17:40:41 +0800

    SpringCache缓存机制

--- spring-context/src/main/java/org/springframework/cache/annotation/CacheEvict.java
+++ spring-context/src/main/java/org/springframework/cache/annotation/CacheEvict.java
@@ -29,6 +29,12 @@ import java.lang.annotation.Target;
  *
  * @author Costin Leau
  * @since 3.1
+
+ 	@CacheEvict是用来标注在需要清除缓存元素的方法或类上的。当标记在一个类上时表示其中所有的方法的执行都会触发缓
+	存的清除操作。@CacheEvict可以指定的属性有value、key、condition、allEntries和beforeInvocation。其中value、key和condition
+	的语义与@Cacheable对应的属性类似。即value表示清除操作是发生在哪些Cache上的（对应Cache的名称）；key表示需要清除的是哪
+	个key，如未指定则会使用默认策略生成的key；condition表示清除操作发生的条件。
+
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
@@ -36,37 +42,29 @@ import java.lang.annotation.Target;
 @Documented
 public @interface CacheEvict {
 
-	/**
-	 * Qualifier value for the specified cached operation.
-	 * <p>May be used to determine the target cache (or caches), matching the qualifier
-	 * value (or the bean name(s)) of (a) specific bean definition.
-	 */
+	// value、key和condition的语义请参照@Cacheable对应的属性
+
 	String[] value();
-
-	/**
-	 * Spring Expression Language (SpEL) attribute for computing the key dynamically.
-	 * <p>Default is "", meaning all method parameters are considered as a key.
-	 */
 	String key() default "";
-
-	/**
-	 * Spring Expression Language (SpEL) attribute used for conditioning the method caching.
-	 * <p>Default is "", meaning the method is always cached.
-	 */
 	String condition() default "";
 
 	/**
-	 * Whether or not all the entries inside the cache(s) are removed or not. By
-	 * default, only the value under the associated key is removed.
-	 * <p>Note that setting this parameter to {@code true} and specifying a {@link #key()}
-	 * is not allowed.
+		清除操作默认是在对应方法成功执行之后触发的，即方法如果因为抛出异常而未能成功返回时也不会触发清除操作。使用
+	 	beforeInvocation可以改变触发清除操作的时间，当我们指定该属性值为true时，Spring会在调用该方法之前清除缓存中的指定元素。
+		@CacheEvict(value="users", beforeInvocation=true)
+		public void delete(Integer id) {
+			System.out.println("delete user by id: " + id);
+		}
 	 */
 	boolean allEntries() default false;
 
 	/**
-	 * Whether the eviction should occur after the method is successfully invoked (default)
-	 * or before. The latter causes the eviction to occur irrespective of the method outcome (whether
-	 * it threw an exception or not) while the former does not.
+		allEntries是boolean类型，表示是否需要清除缓存中的所有元素。默认为false，表示不需要。当指定了allEntries为true时，
+	 	Spring Cache将忽略指定的key。有的时候我们需要Cache一下清除所有的元素，这比一个一个清除元素更有效率。
+		@CacheEvict(value="users", allEntries=true)
+		public void delete(Integer id) {
+			System.out.println("delete user by id: " + id);
+		}
 	 */
 	boolean beforeInvocation() default false;
 }
