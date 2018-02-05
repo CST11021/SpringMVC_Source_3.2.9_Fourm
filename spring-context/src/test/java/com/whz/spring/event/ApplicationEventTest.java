80ff53ceb60db42834638b1e0b64f12a44cb4741
wb-whz291815 <wb-whz291815@alibaba-inc.com>
2018-02-05 15:59:30 +0800

    测试Spring容器事件机制

--- spring-context/src/test/java/com/whz/spring/event/ApplicationEventTest.java
+++ spring-context/src/test/java/com/whz/spring/event/ApplicationEventTest.java
@@ -6,9 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationEventTest {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/whz/spring/event/spring-event.xml");
+		String res = "com/whz/spring/event/spring-event.xml";
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(res);
 		MailSender mailSender = ctx.getBean(MailSender.class);
-		// 邮件发送器发送完一封邮件后会，通过Spring容器向所有监听器发送事件
+		// 邮件发送器发送完一封邮件后会，会通过Spring容器向所有监听器发送一个MailSendEvent事件
 		mailSender.sendMail("王小明");
 	    System.out.println("done.");
 	}
--- spring-context/src/test/java/com/whz/spring/event/MailSendListener.java
+++ spring-context/src/test/java/com/whz/spring/event/MailSendListener.java
@@ -2,7 +2,7 @@ package com.whz.spring.event;
 
 import org.springframework.context.ApplicationListener;
 
-//负责监听 MailSendEvent 事件，一旦Spring容器注册了该事件，就调用onApplicationEvent()方法
+//负责监听 MailSendEvent 事件，一旦Spring容器发布了 MailSendEvent 事件，就调用onApplicationEvent()方法
 public class MailSendListener implements ApplicationListener<MailSendEvent>{
 
 	@Override
